f8da97f3793613dc8003966b0c0fb46329786e70
hlane17 <79113682+hlane17@users.noreply.github.com>
2021-03-12 20:02:38 -0500

    Update fragthresholdPickle.py

--- fragthresholdPickle.py
+++ fragthresholdPickle.py
@@ -1,11 +1,3 @@
-
-
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[59]:
-
-
 import numpy as np
 import h5py
 from matplotlib import pyplot as plt
@@ -25,7 +17,7 @@ G = 4300.7 # gravitational constant in m/s - msun - pc units
 cs = 200 # isothermal sound speed in m/s (= pressure/density, appropriate for ISM at ~10K)
 sigma = 1e3 # surface density M/(pi R^2) in msun pc^-2 (this is arbitrary, just to set the dimensions of our problem - 1000 roughly corresponds to observed cores)
 
-infall_machs = 1.41, #np.logspace(0,3,7,base=2) # the list of infall mach #'s we want - ranges from 1 to 8, evenly spaced in log space (each is a certain % larger than the last, in a geometric progression)
+infall_machs = np.logspace(0,3,7,base=2) # the list of infall mach #'s we want - ranges from 1 to 8, evenly spaced in log space (each is a certain % larger than the last, in a geometric progression)
 alphas = 0, 0.5, 1, 2, 4, 8 #list of turbulent virial parameters we want - 0 is no initial turbulence
 mus = 4, #np.inf, 4, 2, 1, 0.5, 0.25  # list of mass-to-flux ratios (greek letter mu) that we want - infinity is no magnetic field, ~0 is very strong magnetic field
 seeds = 42, #42, 2, 3 # different initial turbulent seed fields - so that we try a few different random samplings of the initial turbulence to make sure results are not a fluke
