606f9ae51c50f8dce52b2e1d63e4adf91d6ce41f
hlane17 <79113682+hlane17@users.noreply.github.com>
2021-06-21 21:44:30 -0400

    Update autoSubmit.py

--- autoSubmit.py
+++ autoSubmit.py
@@ -13,11 +13,11 @@ G = 4300.7 # gravitational constant in m/s - msun - pc units
 cs = 200 # isothermal sound speed in m/s (= pressure/density, appropriate for ISM at ~10K)
 sigma = 1e3 # surface density M/(pi R^2) in msun pc^-2 (this is arbitrary, just to set the dimensions of our problem - 1000 roughly corresponds to observed cores)
 
-infall_machs = np.logspace(0,3,7,base=2) # the list of infall mach #'s we want - ranges from 1 to 8, evenly spaced in log space (each is a certain % larger than the last, in a geometric progression)
+infall_machs = 1.41, #np.logspace(0.5,3,6,base=2) # the list of infall mach #'s we want - ranges from 1 to 8, evenly spaced in log space (each is a certain % larger than the last, in a geometric progression)
 alphas = 0.25, 0.5, 1, 2, 4, 8 #list of turbulent virial parameters we want - 0 is no initial turbulence
-mus = 4, 2, 1, 0.5#np.inf, 4, 2, 1, 0.5, 0.25  # list of mass-to-flux ratios (greek letter mu) that we want - infinity is no magnetic field, ~0 is very strong magnetic field
-seeds = 42, #42, 2, 3 # different initial turbulent seed fields - so that we try a few different random samplings of the initial turbulence to make sure results are not a fluke
-sol_fracs = 0.5,  # 0, 1 # fraction of turbulent field in solenoidal modes 
+mus = 4, 2, 1, 0.5 #np.inf, 4, 2, 1, 0.5, 0.25  # list of mass-to-flux ratios (greek letter mu) that we want - infinity is no magnetic field, ~0 is very strong magnetic field
+seeds = 1,2,3,4,5,6,7,8,9,10,11,12,13,14 # different initial turbulent seed fields - so that we try a few different random samplings of the initial turbulence to make sure results are not a fluke
+sol_fracs = 0, 0.5, 1  # 0, 1 # fraction of turbulent field in solenoidal modes 
 
 for infall_mach in infall_machs:
     for alpha in alphas:
@@ -25,11 +25,11 @@ for infall_mach in infall_machs:
             for seed in seeds:                
                 for sol_frac in sol_fracs:
                     Ngas = max(100 * infall_mach**4, 10000)
-                    run_name = "mach%g_alpha%g_mu%g_sol%g_Res%d_%d"%(round(infall_mach,2), round(alpha,2), round(mu,2), sol_frac, round(Ngas**(1./3)), seed) # this will be the unique identifier for the run - will want to create a new directory with this name
-                    if not isdir(run_name + "/output"):
+                    run_name = "mach%g_alpha%g_mu%g_sol%g_Res%d"%(round(infall_mach,2), round(alpha,2), round(mu,2), sol_frac, round(Ngas**(1./3))) # this will be the unique identifier for the run - will want to create a new directory with this name
+                    if not isfile(run_name + "/slurm*.out"):
                         chdir(run_name)
                         submit_command = "sbatch " + run_name + ".sh"
-                        system(submit_command)
+                        print(submit_command)
 
                         chdir("../") # go back to the top level directory
                     else:
