456d77055d22d5b4c02f344da297e8e059ee8d77
hlane17 <79113682+hlane17@users.noreply.github.com>
2021-06-21 22:03:59 -0400

    Update autoSubmit.py

--- autoSubmit.py
+++ autoSubmit.py
@@ -25,8 +25,8 @@ for infall_mach in infall_machs:
             for seed in seeds:                
                 for sol_frac in sol_fracs:
                     if sol_frac != 0.5 and mu != 4: continue
-                        Ngas = max(250 * infall_mach**4, 100000) # number of gas cells in the core proper - we might as well always have at least 10k, but in Guszejnov 2020 we showed you want to make sure it's at least ~100 Mach^4 as well for things to really be converged
-                        run_name = "mach%g_alpha%g_mu%g_sol%g_Res%d"%(round(infall_mach,2), round(alpha,2), round(mu,2), sol_frac, round(Ngas**(1./3))) # this will be the unique identifier for the run - will want to create a new directory with this name
+                    Ngas = max(250 * infall_mach**4, 100000) # number of gas cells in the core proper - we might as well always have at least 10k, but in Guszejnov 2020 we showed you want to make sure it's at least ~100 Mach^4 as well for things to really be converged
+                    run_name = "mach%g_alpha%g_mu%g_sol%g_Res%d"%(round(infall_mach,2), round(alpha,2), round(mu,2), sol_frac, round(Ngas**(1./3))) # this will be the unique identifier for the run - will want to create a new directory with this name
                         if not path.exists(run_name + "/slurm*.out"):
                             chdir(run_name)
                             submit_command = "sbatch " + run_name + ".sh"
